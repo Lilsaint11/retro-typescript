9104dfac98ba68bf6db6715dc7978bd625ef6a75
Stephen <saintobis11@gmail.com>
2023-12-07 23:25:07 +0100

    secnd commit

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -48,7 +48,7 @@ export default function Main () {
           <h1 className='text-[67px] max-[460px]:text-[57px] max-[400px]:text-[47px] max-[340px]:text-[37px] font-semibold leading-tight sm:text-center  sm:text-[80px] md:text-[100px]  lg:text-[150px]'>2023 RETRO</h1>
           <div className="sm:flex sm:justify-between sm:items-center">
             <p className="text-[14px] font-semibold max-sm:mb-5">Tell your 2023 story!</p>
-            <div className="flex gap-5 items-center">
+            <div className="flex gap-5 max-[350px]:gap-2 items-center">
               <p className={`px-2 py-1 rounded-full border-[1.5px] border-black ${theme == "dark" ? "border-white":"border-black"} text-[12px] max-[400px]:text-[10px] max-[360px]:text-[8px] font-semibold cursor-pointer`} onClick={toggleStoryModalOpen}>Tell your story</p>
 
               <Link href="https://twitter.com/"><p className={`px-2 py-1 rounded-full border-[1.5px] border-black ${theme == "dark" ? "border-white":"border-black"} text-[12px] max-[400px]:text-[10px] max-[360px]:text-[8px] font-semibold cursor-pointer`}>Twitter</p></Link>
